4cda6f8b3d0dda989bb3a5a2044a06fd7df1730a
Tara Rains <tram.nguyenrains@gmail.com>
2023-11-02 10:02:52 +0700

    build whole pipeline testing

--- models/marts/intermediate/int_products.sql
+++ models/marts/intermediate/int_products.sql
@@ -1,7 +1,7 @@
 WITH int_products AS(
     SELECT 
         p.product_id,
-        product_category_name_english as product_category
+        product_category_name_english as product_category --change to allign with column names in downstream
     FROM {{ ref('stg_olist_products') }} as p
     JOIN {{ ref('stg_product_category') }} as c
     ON p.product_category_name = c.product_category_name
